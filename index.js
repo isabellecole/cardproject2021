6d2c52327b6dbe68cee38f82277c580959c60133
izzycole <05398@farnborough.ac.uk>
2021-03-05 09:01:41 +0000

    phase 2 finished

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 "use strict"
 
 let deck = [];
-let colours = ["red","yellow","black"];
 let round = 1;
+let colours = ["red","yellow","black"];
 let body = document.getElementById("body");
-let button = document.getElementById("button")
+let button = document.getElementById("button");
+let cardsPlaying = [];
+let p1Cards =[];
+let p2Cards = [];
+let winner;
 
 
 // Shuffles the pack
@@ -25,9 +29,7 @@ for (let i=0; i<colours.length; i++){
   }
 }
 
-console.log(deck);
 shuffle(deck);
-console.log(deck);
 
 
 //cardlist = document.getElementById("cardlist");
@@ -37,13 +39,18 @@ console.log(deck);
 
 roundBegin1();
 
+// Player 1 "picks up" their card
 function roundBegin1(){
-  document.getElementById("round").innerHTML = "Round: " + round + "<br>";
-  body.innerHTML = "Player 1, it is your turn. <br>";
-  button.innerHTML = "Take a card";
-  button.addEventListener("click",roundBegin2);
+  if (round<=15){
+    if (round>=2){
+      button.removeEventListener("click", roundBegin1);}
+    document.getElementById("round").innerHTML = "Round: " + round + "<br>";
+    body.innerHTML = "Player 1, it is your turn. <br>";
+    button.innerHTML = "Take a card";
+    button.addEventListener("click",roundBegin2);}
 }
 
+// Player 2 "picks up" their cards
 function roundBegin2(){
   body.innerHTML = "Player 2, it is your turn. <br>";
   button.innerHTML = "Take a card";
@@ -51,10 +58,46 @@ function roundBegin2(){
   button.addEventListener("click",showResults);
 }
 
+// The players are told which card they picked up
 function showResults(){
-  button.removeEventListener("click",showResults);
-  let cardsPlaying = [];
+  cardsPlaying = [];
   cardsPlaying.push(deck.shift());
   cardsPlaying.push(deck.shift());
   body.innerHTML = "Player 1 picked up: " + cardsPlaying[0][1] + " of " + cardsPlaying[0][0] + "<br>" + "Player 2 picked up: " + cardsPlaying[1][1] + " of " + cardsPlaying[1][0] + "<br>";
+  button.innerHTML = "Show results";
+  button.removeEventListener("click",showResults);
+  button.addEventListener("click", revealWinner);
+}
+
+// Algorithm to determine who has won. The cards are added to the correct array
+function revealWinner(){
+  if (cardsPlaying[0][0] == cardsPlaying[1][0] ){ //cards are same colour
+    if(cardsPlaying[0][1] > cardsPlaying[1][1]){
+      winner = 1;}
+    else{ winner=2;}
+    body.innerHTML = "Player " + winner + " won because their card is higher.<br>";}
+
+    else { if(cardsPlaying[0][0] == "red"){ //cards are different colours
+    if (cardsPlaying[1][0] == "yellow"){winner = 2;}
+    else{ winner = 1;}} 
+    else{ if(cardsPlaying[0][0] == "yellow"){
+      if (cardsPlaying[1][0] == "black"){winner = 2;}
+      else {winner = 1;}}
+    else{ if(cardsPlaying[0][0] == "black"){
+      if (cardsPlaying[1][0] == "red"){winner = 2;}
+      else { winner=1;}}
+    }}
+    body.innerHTML = "Player " + winner + " won due to colour rules. <br>";}
+
+    if (winner==1){ //adds cards to the array
+      p1Cards.push(cardsPlaying[0]);
+      p1Cards.push(cardsPlaying[1]);
+    } else{
+      p2Cards.push(cardsPlaying[0]);
+      p2Cards.push(cardsPlaying[1]);
+    }
+    button.innerHTML = "Next round";
+    round += 1;
+    button.removeEventListener("click", revealWinner);
+    button.addEventListener("click",roundBegin1);
 }
